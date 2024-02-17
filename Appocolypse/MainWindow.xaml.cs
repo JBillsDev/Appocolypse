9fec9928901c80d1a5268a4d9e7c1471415fe486
JBillsDev <JBillsDev@gmail.com>
2024-02-17 06:15:38 -0700

    Implemented main menu with exit
    Added a main menu with File > Exit to Close() the app.
    Added a binding for background color.
    Added a binding for window title string.
    Added a version number for the app.
    Added OnPropertyChange notification to MainWindow.
    Cleaned up unused lines in .cs files.

--- Appocolypse/MainWindow.xaml.cs
+++ Appocolypse/MainWindow.xaml.cs
@@ -1,24 +1,80 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Appocolypse
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainWindow : Window, INotifyPropertyChanged
 	{
+		#region BackingVariables
+
+		private const string VERSION_NUMBER = "v0.0.0";
+
+		private string _bgColorHex = "#888";
+		private string _titleText = $"Appocolypse {VERSION_NUMBER}";
+
+		#endregion
+
+		#region Properties
+
+		public string BgColorHex
+		{
+			get
+			{
+				return _bgColorHex;
+			}
+
+			private set
+			{
+				_bgColorHex = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public string TitleText
+		{
+			get
+			{
+				return _titleText;
+			}
+
+			private set
+			{
+				_titleText = value;
+				OnPropertyChanged();
+			}
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+
+		#endregion
+
+		#region Constructors
+
 		public MainWindow ()
 		{
+			DataContext = this;
 			InitializeComponent();
 		}
+
+		#endregion
+
+		#region Private Methods
+
+		private void OnPropertyChanged ( [CallerMemberName] string propertyName = "" )
+		{
+			PropertyChanged?.Invoke( this, new PropertyChangedEventArgs( propertyName ) );
+		}
+
+		#endregion
+
+		#region Private Methods - Clicked
+
+		private void MainMenuItem_ExitClicked ( object sender, RoutedEventArgs e )
+		{
+			Close();
+		}
+
+		#endregion
 	}
 }
